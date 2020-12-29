3960d4de82204931a5a0b88d5bbc09159e5d066a
dtn1 <david.newton@nist.gov>
2020-12-29 08:00:21 -0700

    fixed bug when you changed from fixed/not fixed mid use

--- CharpyApp/R/plotModules.R
+++ CharpyApp/R/plotModules.R
@@ -175,7 +175,7 @@ plotResidsServer <- function(id,computedResults) {
         ns <- session$ns
         mods = computedResults()$mstats$mod
         myl = as.list(mods)
-        names(myl) = correct_names(mods)
+        names(myl) = correct_names(mods) #e.g. 'HT':htf
         selectInput(ns('which_model'),'Model',choices=myl)
       })
       
@@ -184,73 +184,11 @@ plotResidsServer <- function(id,computedResults) {
         other_vars = computedResults()$other_vars
         model_name = input$which_model
         res = computedResults()$results[[model_name]]
+        if(is.null(res)) {
+          return(NULL)
+        }
         fun = get(model_name)
         nlsres(other_vars$yy,other_vars$temp,model_name,res,fun,other_vars$lower_shelf,other_vars$upper_shelf,other_vars$fit)
-      })
-      
-      output$resid_plot = renderPlot({
-        # this is currently not used, but didn't want to delete
-        req(input$which_model)
-        
-        mstats = computedResults()$mstats
-        results = computedResults()$results
-        other_vars = computedResults()$other_vars
-        #other_vars: mod, temp, yy, nn, n.new, nsim, uls, uus, fit, yval, t, newt,
-        #            upper_shelf, lower_shelf
-        # could attach the list, but might be sloppy coding
-        
-        mod = other_vars$mod[other_vars$mod %in% input$which_model]
-        temp = other_vars$temp
-        yy = other_vars$yy
-        nn = other_vars$nn
-        newt = other_vars$newt
-        lower_shelf = other_vars$lower_shelf
-        upper_shelf = other_vars$upper_shelf
-        
-        # prepare df for plotting
-        original_data = data.frame(yy=yy,temp=temp)
-        num_models = length(mod)
-        df_to_plot = data.frame(temp = temp,
-                                yy = yy,
-                                value = 0,
-                                model = mod)
-        
-        
-
-        model_name = input$which_model
-        myfun = eval(parse(text=model_name))
-        inds = df_to_plot$model == model_name
-        
-        if(grepl('uf$',model_name)) {
-          df_to_plot$value[inds] = myfun(coef(results[[model_name]]), 
-                                         df_to_plot$temp, 
-                                         other_vars$upper_shelf)
-          
-        } else if(grepl('f$',model_name)) {
-          df_to_plot$value[inds] = myfun(coef(results[[model_name]]), 
-                                         df_to_plot$temp,
-                                         other_vars$lower_shelf,
-                                         other_vars$upper_shelf)
-          
-        } else {
-          df_to_plot$value[inds] = myfun(coef(results[[model_name]]),
-                                         df_to_plot$temp)
-        }
-        
-         
-          
-        yvar_name = c('KV','LE','SFA')[other_vars$fit]
-        
-        p = ggplot(data=df_to_plot,aes(x=temp,y=yy-value)) + 
-          geom_point() +
-          geom_hline(yintercept=0) +
-          ggtitle(paste(yvar_name,': Residual Plot',sep='')) +
-          theme(plot.title = element_text(hjust = 0.5)) +
-          ylab("Residual") +
-          xlab("Temperature")
-        
-        p
-        
       })
       
     }

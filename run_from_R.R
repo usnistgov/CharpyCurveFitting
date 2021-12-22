c19feb91c96ffc00c6e8f430dfc1aca25a62483c
dtn1 <david.newton@nist.gov>
2021-12-22 13:05:53 -0700

    edit run_from_R

--- run_from_R.R
+++ run_from_R.R
@@ -1,6 +1,12 @@
-if (!require("pacman")) install.packages("pacman")
+packages = c('shiny','shinythemes','readr','stringr','dplyr','minpack.lm',
+            'ggplot2','pracma','truncnorm','tinytex','DT')
 
-pacman::p_load('shiny','shinythemes','readr','stringr','dplyr','minpack.lm',
-               'ggplot2','pracma','truncnorm','tinytex','DT')
+needed_packages = packages[!(packages %in% installed.packages())]
+
+if(length(needed_packages) > 0) {
+  install.packages(needed_packages)
+}
+
+library(shiny)
 
 runGitHub('CharpyCurveFitting','usnistgov')
